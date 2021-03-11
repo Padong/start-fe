761af30f3b3b07d2e6671c7f5d799ac33cbec793
Padong <nyc80886@naver.com>
2021-03-11 10:12:56 +0900

    3월 1주차 과제

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('app.js');
+// console.log('app.js');
+
+
+// console.log(1);
+// console.log(2);
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+// };
+
+// var image1 =[1, 2, 3, 4, 5, 6, 'a', {}];
+// image1[0]= 'a';
+
+// var data = null;
+// var data1 = '';
+// var data2;
+
+// console.log(data, data1, data2);
+
+// console.log(image, image1);
+
+
+// console.log(1+2);
+// console.log('a'+'b');
+// console.log(1 + '4');
+
+// var ishero = true;
+
+// if (!ishero) {
+//     console.log('hero');
+// }
+
+console.log(typeof 1);
+console.log(typeof []);
+console.log(typeof '1');
+console.log(typeof {});
